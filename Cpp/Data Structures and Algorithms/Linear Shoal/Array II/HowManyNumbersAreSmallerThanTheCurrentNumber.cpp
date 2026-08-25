643a2fb2731390db5bb25edb1dfd559ab74e021e
HassanPls <plmvfilho@gmail.com>
2026-08-25 15:23:37 -0300

    feat: adding solution for "Q2. How Many Numbers Are Smaller Than the Current Number"

--- Cpp/Data Structures and Algorithms/Linear Shoal/Array II/HowManyNumbersAreSmallerThanTheCurrentNumber.cpp
+++ Cpp/Data Structures and Algorithms/Linear Shoal/Array II/HowManyNumbersAreSmallerThanTheCurrentNumber.cpp
@@ -4,15 +4,31 @@ using namespace std;
 class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
-        vector<int> maiores(500, 0);
+        vector<int> maiores(101, 0);
         vector<int> ans;
 
-        sort(nums.begin(), nums.end());
+        vector<int> sortedNums = nums;
+        sort(sortedNums.begin(), sortedNums.end());
 
-        for (int i = 0; i < nums.size(); i++)
+        maiores[sortedNums[0]] = 0;
+
+        for (int i = 1; i < nums.size(); i++)
         {
+            int index = i;
+
+            while (index > 0 && sortedNums[index - 1] == sortedNums[index])
+            {
+                index--;
+            }
             
+            maiores[sortedNums[i]] = index;
         }
         
+        for (int i = 0; i < nums.size(); i++)
+        {
+            ans.push_back(maiores[nums[i]]);
+        }
+        
+        return ans;
     }
 };
